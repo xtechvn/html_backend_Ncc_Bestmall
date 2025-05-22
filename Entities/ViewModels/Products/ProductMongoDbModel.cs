8f02ba9645d49a022c511364893edc3c19efc61f
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-05-22 17:29:07 +0700

    fix model

--- Entities/ViewModels/Products/ProductMongoDbModel.cs
+++ Entities/ViewModels/Products/ProductMongoDbModel.cs
@@ -57,7 +57,7 @@ namespace Entities.ViewModels.Products
         public string description_ingredients { get; set; }
         public string description_effect { get; set; }
         public string description_usepolicy { get; set; }
-        public int? review_count { get; set; }
+        public double? review_count { get; set; }
         public float? rating { get; set; }
         public long? total_sold { get; set; }
 
